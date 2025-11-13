771cc0cf4a101254382341fdc2ada7103736c2de
jovven <shadowguy8888@gmail.com>
2025-11-13 12:03:53 +0800

    Add GitHub Actions CI Pipeline

---
+++ tests/ui.spec.js
@@ -0,0 +1,13 @@
+const { test, expect } = require('@playwright/test'); 
+
+ 
+
+test('button click updates message', async ({ page }) => { 
+
+  await page.goto('file://' + __dirname + '/../index.html'); 
+
+  await page.click('#clickMe'); 
+
+  await expect(page.locator('#message')).toHaveText('Button clicked!');
+
+});

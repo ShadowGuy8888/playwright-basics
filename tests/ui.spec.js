8f806d5ba053400e10532abce8fc4dbdbc83e9db
jovven <shadowguy8888@gmail.com>
2025-11-13 12:14:01 +0800

    2nd commit

--- tests/ui.spec.js
+++ tests/ui.spec.js
@@ -4,6 +4,7 @@ const { test, expect } = require('@playwright/test');
 
 test('button click updates message', async ({ page }) => { 
 
+  // eslint-disable-next-line no-undef
   await page.goto('file://' + __dirname + '/../index.html'); 
 
   await page.click('#clickMe'); 
